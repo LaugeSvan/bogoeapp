7b7dc820073983fc08f3445e8a9d3f5728b65430
SalsaCat <laugessvan@gmail.com>
2026-04-22 18:14:00 +0200

    Remove header from welcome screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
+import { Stack } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 import styles from "../styles/global";
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Stack.Screen options={{ headerShown: false }} />;
       <Text style={styles.text}>Velkommen til appen.</Text>
       <TouchableOpacity style={styles.btn}>
         <Text style={styles.text}>Opret</Text>
