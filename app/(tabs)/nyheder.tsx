dee57ed0354008ff3fb1bfc7e8def28213d7e3b1
SalsaCat <laugessvan@gmail.com>
2026-04-23 16:00:52 +0200

    fix error 40

--- app/(tabs)/nyheder.tsx
+++ app/(tabs)/nyheder.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from "react-native";
 import styles from "../../styles/global";
 
-export default function Profil() {
+export default function Nyheder() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Profil</Text>
+      <Text style={styles.text}>Nyheder</Text>
     </View>
   );
 }
