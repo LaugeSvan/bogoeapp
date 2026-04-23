83e46db8b37f8ea1d34b0551c53bc64a77ec6413
SalsaCat <laugessvan@gmail.com>
2026-04-23 11:31:51 +0200

    Add basic form

--- app/login.tsx
+++ app/login.tsx
@@ -1,10 +1,17 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import styles from "../styles/global";
 
 const Login = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Login</Text>
+      <Text style={styles.text}>Login på vores fælleskab!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Email" />
+        <TextInput style={styles.input} placeholder="Kodeord" />
+        <TouchableOpacity style={styles.welcomeBtn}>
+          <Text style={styles.text}>Log ind</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
