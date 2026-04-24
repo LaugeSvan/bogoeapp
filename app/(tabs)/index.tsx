69ae78a79cb84e843faaa638684d50ad494f9258
SalsaCat <laugessvan@gmail.com>
2026-04-24 20:47:43 +0200

    Fix!!

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,40 +1,13 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
-import { useEffect, useState } from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import styles from "../../styles/global";
-
-interface User {
-  name: string;
-  email: string;
-  password: string;
-  isOnBogø: boolean;
-}
 
 const HomeScreen = () => {
   const router = useRouter();
-  const [user, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    const loadUser = async () => {
-      const currentUser = await AsyncStorage.getItem("currentUser");
-      if (currentUser) {
-        setUser(JSON.parse(currentUser));
-      } else {
-        router.replace("/");
-      }
-    };
-    loadUser();
-  }, [router]);
-
-  if (!user) return null;
-
   return (
-    <SafeAreaView style={styles.safeAreaView}>
-      <View style={styles.container}>
-        <Text style={styles.text}>Hjem</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hjem</Text>
+    </View>
   );
 };
 
